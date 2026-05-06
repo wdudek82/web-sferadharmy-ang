7e0f084f451af150fc1b221f095a22974203d28d
Wojciech Dudek <wdudek82@gmail.com>
2026-05-06 16:46:12 +0200

    SD-6: Update about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AboutComponent } from './pages/about/about.component';
 import { ArticleComponent } from './pages/article/article.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { EventsComponent } from './pages/events/events.component';
-import { HomeComponent } from './pages/home/home.component';
 import { TextsComponent } from './pages/texts/texts.component';
 
 export const routes: Routes = [
