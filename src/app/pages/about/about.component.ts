ba0fc94e09f8d66653557f7aa1388797a4b458a8
Wojciech Dudek <wdudek82@gmail.com>
2026-05-16 15:31:19 +0200

    SD-7: Split "o nas" into three sections

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
+  imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {}
